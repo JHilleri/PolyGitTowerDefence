35e1f25f925d6faf6402eff87ba38703883709d8
Ethios <cyril.richard95@gmail.com>
2016-10-09 11:08:48 -0400

    Ajout d'un soldat capable de suivre un chemin (et d'un chemin de test)

---
+++ Assets/Scripts/Soldat.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soldat : MonoBehaviour {
+
+    public int chemin;
+    public int vieMax;
+    public float vitesse;
+    public float marge;
+    private int vie;
+    private int etape;
+    private pointPassage objectif;
+    private float distanceX;
+    private float distanceY;
+    private float distance;
+    private float vitesseX;
+    private float vitesseY;
+
+	// Use this for initialization
+	void Start () {
+        etape = 0;
+        objectif = null;
+        vie = vieMax;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (objectif == null)
+        {
+            pointPassage[] points = Object.FindObjectsOfType<pointPassage>();
+            bool found = false;
+            int indexPoints = 0;
+            while(indexPoints < points.Length && !found)
+            {
+                if (points[indexPoints].numeroChemin == chemin && points[indexPoints].ordre == etape)
+                {
+                    objectif = points[indexPoints];
+                    distanceX = objectif.transform.position.x - transform.position.x;
+                    distanceY = objectif.transform.position.y - transform.position.y;
+                    distance = Mathf.Sqrt(distanceX * distanceX + distanceY * distanceY);
+                    vitesseX = (distanceX / distance) * vitesse;
+                    vitesseY = (distanceY / distance) * vitesse;
+                    found = true;
+                }
+                indexPoints++;
+            }
+            if (!found)
+            {
+                gagne();
+            }
+        }
+        distanceX = objectif.transform.position.x - transform.position.x;
+        distanceY = objectif.transform.position.y - transform.position.y;
+        distance = Mathf.Sqrt(distanceX * distanceX + distanceY * distanceY);
+        if(distance < marge)
+        {
+            etape++;
+            objectif = null;
+        }
+        else
+        {
+            transform.Translate(vitesseX, vitesseY, 0);
+        }
+        if(vie <= 0)
+        {
+            meurt();
+        }
+    }
+
+    void gagne()
+    {
+        Destroy(transform.root.gameObject);
+    }
+
+    void meurt()
+    {
+        Destroy(transform.root.gameObject);
+    }
+}
---
+++ Assets/Scripts/pointPassage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pointPassage : MonoBehaviour {
+
+    public int numeroChemin;
+    public int ordre;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
