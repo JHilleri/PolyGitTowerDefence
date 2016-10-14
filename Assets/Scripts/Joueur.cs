aa2d6cae02d41c8debfb66910b5eb585ea40aaeb
Eibu <florian.portailler@etu.univ-tours.fr>
2016-10-14 16:55:55 -0400

    + Création Joueurs (vie, argent, expérience) et Partie(joueur1, joueur2, update des joueurs, partie solo ou multi(booléen))

---
+++ Assets/Scripts/Joueur.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Joueur : MonoBehaviour {
+	
+	public int vie;
+	public int argent;
+	public int experience;
+	public int camp;
+	public Text vieText;
+	public Text argentText;
+
+	// Use this for initialization
+	void Start () {
+		vieText = GameObject.FindGameObjectsWithTag("VieJoueur")[0].GetComponent<Text>();
+        vieText.text = vie.ToString();
+		argentText = GameObject.FindGameObjectsWithTag("Argent")[0].GetComponent<Text>();
+        argentText.text = argent.ToString();
+		
+	}
+	
+	public void SetCamp(int c) {
+		camp = c;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/Partie.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Partie : MonoBehaviour {
+
+	public int typePartie;//Solo/gauche (0), multi/droit (1)
+	public Joueur joueurGauche;
+	public Joueur joueurDroit;
+    Text pdvAdverse;
+	// Use this for initialization
+	void Start () {
+        pdvAdverse = GameObject.FindGameObjectsWithTag("VieAdversaire")[0].GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(typePartie==0) {
+			pdvAdverse.text = joueurDroit.vie.ToString();
+            joueurGauche.vieText.text = joueurGauche.vie.ToString();
+            joueurGauche.argentText.text = joueurGauche.argent.ToString();
+        }
+        else
+        {
+            pdvAdverse.text = joueurGauche.vie.ToString();
+            joueurDroit.vieText.text = joueurDroit.vie.ToString();
+            joueurDroit.argentText.text = joueurDroit.argent.ToString();
+        }
+	}
+}
