a8b36ad41fce932d85b67c54c60f4daa73ced343
Pierre <pierre.fervault@laposte.net>
2016-10-13 22:28:33 -0400

    Modification Interface de Base, Ajout Element pour création de tours

---
+++ Assets/Scripts/UIColorElement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIColorElement : MonoBehaviour {
+
+    public Element element;
+
+    // Use this for initialization
+    void Start () {
+        Image image = this.GetComponent<Image>();
+        image.color = element.couleur;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
