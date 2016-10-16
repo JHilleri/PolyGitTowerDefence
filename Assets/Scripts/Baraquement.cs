b19681c93c251fd9c2e68241f73e437e00b8e58d
Kévin <Kvin>
2016-10-16 15:37:06 -0400

    Ajout baraquement

--- Assets/Scripts/Baraquement.cs
+++ Assets/Scripts/Baraquement.cs
@@ -8,11 +8,10 @@ public class Baraquement : MonoBehaviour {
     public GameObject AjoutUnite;
     public int camp; // définit le camp auquel la tour appartient 1 -> gauche, 2 -> droite
     public int cout; // Cout de la caserne
-	public int classe = 3; // archer = 0 - brute = 1 - magicien = 2 - paysan = 3 - soldat = 4 - tank = 5, par défaut on produit des paysans
-    public Element element;
+	 public Element element;
     public Sprite image;
     public Sprite imageCouleur;
-    public int intervalle;
+    //public int intervalle;
 	
     private SpriteRenderer spriteRenderer;
     private SpriteRenderer colorSpriteRenderer;
@@ -54,6 +53,7 @@ public class Baraquement : MonoBehaviour {
             else
             {
                 creationUnite();
+                compteur = 0;
             }
         }
 	}
@@ -64,16 +64,15 @@ public class Baraquement : MonoBehaviour {
         menuActif = true;
     }
 
-    void creationUnite() // les unités créées se trouvent sur le baraquement (elles vont d'elles mêmes prendre leur chemin)
+    /*void creationUnite() // les unités créées se trouvent sur le baraquement (elles vont d'elles mêmes prendre leur chemin)
     {
         GameObject Unite = Instantiate(AjoutUnite);
-        Unite.GetComponent<SpriteRenderer>().color = element.couleur;
         Unite.transform.parent = transform;
-		Soldat script = Unite.GetComponent<Soldat>(); // l'unité est déployé mais n'a pas encore de classe
-		script.classe = classe; // attribution de la classe du baraquement à l'unité tout juste créée
+        Unite.transform.position = transform.position;
+        Soldat script = Unite.GetComponent<Soldat>(); // l'unité est déployé mais n'a pas encore de classe
         script.element = element;
         script.camp = camp;
-    }
+    }*/
 
     public void OnPauseGame()
     {
