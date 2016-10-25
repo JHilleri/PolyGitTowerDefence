93fa8cd42ce3546f9219ba05dffe69c685c919ac
Ethios <cyril.richard95@gmail.com>
2016-10-25 00:05:52 -0400

    Debut de travail sur tours de feu AOE

---
+++ Assets/Scripts/ZoneEffet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu()]
+public class ZoneEffet : ScriptableObject {
+
+    public float degat;
+    public float rayon;
+    public Element element;
+    public int camp;
+    public Tour tour;
+
+	public void active () {
+        Soldat[] listeSoldats = FindObjectsOfType<Soldat>();
+        foreach(Soldat soldat in listeSoldats)
+        {
+            if (soldat.camp != camp && tour.distance(soldat.gameObject) < rayon)
+            {
+                soldat.degat(soldat.element.lireRatioDegat(element) * degat);
+            }
+        }
+	}
+}
