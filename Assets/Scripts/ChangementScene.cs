bb94a07c97f15c908a4281dad40e0b27a2aa43d0
Ethios <cyril.richard95@gmail.com>
2016-10-30 16:53:47 -0400

    Ajout d'un menu principal moche mais fonctionnel

---
+++ Assets/Scripts/ChangementScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangementScene : MonoBehaviour{
+
+    public string nomScene;
+
+    public void chargeScene()
+    {
+        SceneManager.LoadScene(nomScene);
+    }
+
+}
